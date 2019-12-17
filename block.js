9ae1e3067f9de438bc0a10e42994aa2d3e95b52f
munyuhuwa <super.hyper.luck@gmail.com>
2019-12-17 22:58:32 +0900

    test async

--- block.js
+++ block.js
@@ -1,34 +1,50 @@
-var canvas = document.getElementById('canvas')
-var context = canvas.getContext('2d')
-// context.fillRect(30, 30, 100, 100)
-
-function render ()
+class BasicObject
 {
-    context.clearRect(0, 0, canvas.clientWidth, canvas.height)
-}
-
-class Block
-{
-    constructor ()
+    constructor()
     {
         this.pos = [0, 0]
         this.size = [10, 10]
     }
+
+    center()
+    {
+        return [
+            (this.pos[0] +(this.size[0] / 2)),
+            (this.pos[1] +(this.size[1] / 2)),
+        ]
+    }
 }
 
-class Ball
+class DynamicObject extends BasicObject
 {
-    constructor ()
+    constructor()
     {
-        this.pos = [0, 0]
-        this.dir = [0, 0]
-        this.size = [10, 10]
+        super()
+        this.dir = [10, 10]
     }
+}
+
+class Block extends BasicObject
+{
+    constructor()
+    {
+        super()
+    }
+}
+
+
+class Ball extends DynamicObject
+{
+    constructor()
+    {
+        super()
+    }
+
 }
 
 class Field
 {
-    constructor ()
+    constructor()
     {
         this.width = 800
         this.height = 800
@@ -36,13 +52,49 @@ class Field
         this.balls = []
     }
 
-    initBlocks ()
+    initBlocks()
     {
         
     }
     
-    moveBalls ()
+    moveBalls()
     {
 
     }
 }
+
+class Canvas
+{
+    constructor()
+    {
+        this.canvas = document.getElementById('canvas')
+        this.context = this.canvas.getContext('2d')
+        // context.fillRect(30, 30, 100, 100)
+    }
+
+    clear()
+    {
+        this.context.clearRect(0, 0, this.canvas.clientWidth, this.canvas.height)
+    }
+
+    render()
+    {
+        this.clear()
+        console.log('bbb')
+    }
+
+    async play()
+    {
+        console.log('aaa')
+        await new Promise(resolve => {
+            setTimeout(() => {
+                this.render()
+                resolve()
+            }, 500);
+        });
+        console.log('ccc')
+    }
+}
+
+canvas = new Canvas()
+canvas.play()
