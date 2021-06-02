8a3f738de9657a1ca9a22090db8d8557d465b584
munyuhuwa <mysterious.bird@outlook.jp>
2021-06-02 16:20:38 +0900

    デザイン調整

--- js/MahjongSlogan.js
+++ js/MahjongSlogan.js
@@ -15,7 +15,8 @@ function prepareSloganParts() {
 					sloganParts[7].push(row[1]);
 				}
 			}
-		});
+		})
+		.then(updateSlogan);
 }
 
 function drawGacha(len) {
@@ -26,42 +27,44 @@ function drawGacha(len) {
 	return sloganParts[len][index];
 }
 
+function updateSlogan() {
+	let $slogan = document.querySelectorAll('.slogan');
+	let $protectSwitch = document.querySelectorAll('.protect-switch');
+	let ku = '';
+	const lens = { 0: 5, 1: 7, 2: 5, };
+	for (let i = 0; i < Object.keys(lens).length; ++i) {
+		let part;
+		if ($protectSwitch[i].checked) {
+			part = $slogan[i].textContent;
+		} else {
+			part = drawGacha(lens[i]);
+		}
+		ku += part + ' ';
+		$slogan[i].textContent = part;
+	}
+	console.log(ku);
+	ku += '\n【麻雀標語ガチャ】';
+	$tweetContainer = document.getElementById('tweet-container');
+	while ($tweetContainer.firstChild) {
+		$tweetContainer.removeChild($tweetContainer.firstChild);
+	}
+	twttr.widgets.createShareButton(
+		"",
+		$tweetContainer,
+		{
+			size: "large",
+			text: ku,
+			via: "munyuhuwa",
+		}
+	);	
+}
+
 function loadFinished() {
 	// 標語パーツを読み込む
 	prepareSloganParts();
 	// イベントをバインド
 	document.querySelectorAll('.gacha').forEach(target => {
-		target.addEventListener('click', () => {
-			let $slogan = document.querySelectorAll('.slogan');
-			let $protectSwitch = document.querySelectorAll('.protect-switch');
-			let ku = '';
-			const lens = { 0: 5, 1: 7, 2: 5, };
-			for (let i = 0; i < Object.keys(lens).length; ++i) {
-				let part;
-				if ($protectSwitch[i].checked) {
-					part = $slogan[i].textContent;
-				} else {
-					part = drawGacha(lens[i]);
-				}
-				ku += part + ' ';
-				$slogan[i].textContent = part;
-			}
-			console.log(ku);
-			ku += '\n【麻雀標語ガチャ】';
-			$tweetContainer = document.getElementById('tweet-container');
-			while ($tweetContainer.firstChild) {
-				$tweetContainer.removeChild($tweetContainer.firstChild);
-			}
-			twttr.widgets.createShareButton(
-				"",
-				$tweetContainer,
-				{
-					size: "large",
-					text: ku,
-					via: "munyuhuwa",
-				}
-			);
-		});
+		target.addEventListener('click', updateSlogan);
 	});
 };
 
